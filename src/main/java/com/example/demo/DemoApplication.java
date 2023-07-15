9eb0607eaa1aa08ebbbc8603555032790b6764ab
Tomáš Čížek <tomascizek@gmail.com>
2023-07-15 20:51:27 +0200

    Allow main application to accept two parameters and output desired input

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
+import com.example.demo.library.filereader.FileLineReader;
+import java.io.IOException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
-  }
+    if (args.length != 2) {
+      throw new RuntimeException("Invalid number of params. There should be exactly two: <file> <line_number>");
+    }
 
+    String filePath = args[0];
+    int lineNumber = Integer.parseInt(args[1]);
+    FileLineReader reader = new FileLineReader();
+
+    try {
+      String line = reader.readLineInFile(filePath, lineNumber);
+      System.out.println(line);
+    } catch (IOException e) {
+
+      throw new RuntimeException("Error reading file: " + e.getMessage());
+    }
+  }
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,12 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +14,55 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoApplicationTests {
 
   @Test
-  void contextLoads() {
+  void testItThrowsErrorOnInvalidParams() {
+    String testFilePath = "src/test/resources/testFile.txt";
+    String[] args = {testFilePath};
+
+    Exception exception = assertThrows(RuntimeException.class, () -> {
+      DemoApplication.main(args);
+    });
+    String expectedMessage = "Invalid number of params";
+    assertTrue(exception.getMessage().contains(expectedMessage));
+  }
+
+  @Test
+  void testItThrowsErrorOnInvalidFile() {
+    String testFilePath = "src/test/resources/nonExistingFile.txt";
+    String[] args = {testFilePath, "2"};
+
+    Exception exception = assertThrows(RuntimeException.class, () -> {
+      DemoApplication.main(args);
+    });
+    String expectedMessage = "Error reading file";
+    assertTrue(exception.getMessage().contains(expectedMessage));
+  }
+  
+  @Test
+  void testItPrintsOutResult() {
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
+
+    String testFilePath = "src/test/resources/testFile.txt";
+    String[] args = {testFilePath, "2"};
+
+    DemoApplication.main(args);
+
+    cleanupFileIndex(testFilePath);
+    assertEquals("grape" + System.lineSeparator(), outContent.toString());
+    assertEquals("", errContent.toString());
+  }
+
+  private static void cleanupFileIndex(String indexFilePath) {
+    File indexFile = new File(getIndexPath(indexFilePath));
+    if (indexFile.exists()) {
+      indexFile.delete();
+    }
+  }
+  
+  private static String getIndexPath(String indexFilePath) {
+    return indexFilePath + ".idx";
   }
 
 }
