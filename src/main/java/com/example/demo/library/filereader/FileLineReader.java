a33dd3f51ce09ce92373fd2839ff99cc27e89101
Tomáš Čížek <tomascizek@gmail.com>
2023-07-17 10:37:56 +0200

    refactoring

--- src/main/java/com/example/demo/library/filereader/FileLineReader.java
+++ src/main/java/com/example/demo/library/filereader/FileLineReader.java
@@ -12,20 +12,28 @@ public class FileLineReader {
       createIndexFile(filePath, indexFile);
     }
 
-    // Read the byte offset from the index file
+    long byteOffset = getByteOffset(lineNumber, indexFile);
+
+    return getLineByOffset(filePath, byteOffset);
+  }
+
+  private static long getByteOffset(int lineNumber, File indexFile) throws IOException {
     long byteOffset;
     try (RandomAccessFile raf = new RandomAccessFile(indexFile, "r")) {
       raf.seek(lineNumber * Long.BYTES);
       byteOffset = raf.readLong();
     }
+    return byteOffset;
+  }
 
+  private static String getLineByOffset(String filePath, long byteOffset) throws IOException {
     try (RandomAccessFile raf = new RandomAccessFile(filePath, "r")) {
       raf.seek(byteOffset);
       return raf.readLine();
     }
   }
 
-  private void createIndexFile(String filePath, File indexFile) throws IOException {
+  private static void createIndexFile(String filePath, File indexFile) throws IOException {
     try (
         RandomAccessFile inputFile = new RandomAccessFile(filePath, "r");
         RandomAccessFile idxFile = new RandomAccessFile(indexFile, "rw")
